f6e9d9841d438b49901226cf1fbd1202f3cc11a0
vennasaur <Vennasaur@gmail.com>
2024-04-20 12:56:03 -0400

    made keywords work

--- keywords.py
+++ keywords.py
@@ -1,27 +1,28 @@
-import openai
+from openai import OpenAI
 import json
 # Load the API key from an environment variable
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # This method loads variables from a .env file into the environment
-api_key = os.getenv('OPENAI_API_KEY')
+load_dotenv() 
 
-openai.api_key = apiKey
 
 client = OpenAI()
+
 
 def text_to_keywords(text: str) -> dict[str, str]:
     
     prompt = "Turn this transcript below surrounded by {} into only a JSON dictionary with no other words, containing pairs with the keyword and the definition of that keyword in the format of {term1: definition1, term2: definition2} with the terms and definitions being strings " +"{"+text+"}"
-    response = client.ChatCompletion.create(
+    response = client.chat.completions.create(
     model="gpt-4-turbo",
-    max_tokens=120,
     response_format={ "type": "json_object" },
     messages=[
         {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
         {"role": "user", "content": prompt}
         ]
     )
-    json.loads(response['choices'][0]['message']['content'])#this should be our response
+    print(response.choices[0].message.content)#this should be our response
     pass
+
+thing = input("put ur prompt here\n")
+text_to_keywords(thing)
