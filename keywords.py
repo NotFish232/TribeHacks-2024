fbd2e7876ca450b842dd1b0efafddd10413016e6
vennasaur <Vennasaur@gmail.com>
2024-04-20 12:26:56 -0400

    added keyword

--- keywords.py
+++ keywords.py
@@ -1,11 +1,20 @@
-from openai import OpenAI
+import openai
+
+# Load the API key from an environment variable
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This method loads variables from a .env file into the environment
+api_key = os.getenv('OPENAI_API_KEY')
+
+openai.api_key = apiKey
 
 client = OpenAI()
 
 def text_to_keywords(text: str) -> dict[str, str]:
     
     prompt = "Turn this transcript below surrounded by {} into only a JSON dictionary with no other words, containing pairs with the keyword and the definition of that keyword in the format of {term1: definition1, term2: definition2} with the terms and definitions being strings " +"{"+text+"}"
-    response = client.chat.completions.create(
+    response = client.ChatCompletion.create(
     model="gpt-4-turbo",
     response_format={ "type": "json_object" },
     messages=[
@@ -13,6 +22,6 @@ def text_to_keywords(text: str) -> dict[str, str]:
         {"role": "user", "content": prompt}
         ]
     )
-    print(response.choices[0].message.content)#this should be our response
+    return response['choices'][0]['message']['content']#this should be our response
     pass
 
