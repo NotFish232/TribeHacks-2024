3f72570244f83030a3a260550bd045d48d3fb72f
vennasaur <Vennasaur@gmail.com>
2024-04-20 12:32:58 -0400

    updated keywords

--- keywords.py
+++ keywords.py
@@ -1,5 +1,5 @@
 import openai
-
+import json
 # Load the API key from an environment variable
 from dotenv import load_dotenv
 import os
@@ -16,12 +16,12 @@ def text_to_keywords(text: str) -> dict[str, str]:
     prompt = "Turn this transcript below surrounded by {} into only a JSON dictionary with no other words, containing pairs with the keyword and the definition of that keyword in the format of {term1: definition1, term2: definition2} with the terms and definitions being strings " +"{"+text+"}"
     response = client.ChatCompletion.create(
     model="gpt-4-turbo",
+    max_tokens=120,
     response_format={ "type": "json_object" },
     messages=[
         {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
         {"role": "user", "content": prompt}
         ]
     )
-    return response['choices'][0]['message']['content']#this should be our response
+    json.loads(response['choices'][0]['message']['content'])#this should be our response
     pass
-
