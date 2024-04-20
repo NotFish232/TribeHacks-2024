4757e39befc661a88e8390c583c366ee4fcfe4a2
Alan Jiang <alanzjiang10@gmail.com>
2024-04-20 16:18:37 -0400

    Transcription changed to openAI whisper, now quicker.

--- src/transcription.py
+++ src/transcription.py
@@ -1,41 +1,21 @@
-import speech_recognition as sr # type: ignore
-import os 
-from pydub import AudioSegment
-# import time
+from openai import OpenAI
+import time
 
-def chunked_audio_to_text(audio_name) -> str:
-    r = sr.Recognizer()
-    with sr.AudioFile(audio_name) as source:
-        audio_data = r.record(source)
-      
-        text = r.recognize_google(audio_data)
+def audio_to_text(path) -> str:
+    client = OpenAI()
 
-        return text
-    
-def audio_to_text(path, minutes=1/6):
-    """Splitting the large audio file into fixed interval chunks
-    and apply speech recognition on each of these chunks"""
-    sound = AudioSegment.from_file(path)  
-    chunk_length_ms = int(1000 * 60 * minutes)
-    chunks = [sound[i:i + chunk_length_ms] for i in range(0, len(sound), chunk_length_ms)]
-    folder_name = "audio-chunks"
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
-    whole_text = ""
-    # process each chunk 
-    for i, audio_chunk in enumerate(chunks, start=1):
-        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
-        audio_chunk.export(chunk_filename, format="wav")
-        try:
-            text = chunked_audio_to_text(chunk_filename)
-        except sr.UnknownValueError as e:
-            print("Error:", str(e))
-        else:
-            text = f"{text.capitalize()}. "
-            # print(chunk_filename, ":", text)
-            whole_text += text
-    return whole_text
+    audio_file= open(path, "rb")
+
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file
+    )
+
+    return transcription.text
 
 # start = time.time()
-# print(audio_to_text("7601-291468-0006.wav"))
+# # text = audio_to_text("presentation.wav")
+# text = audio_to_text("presentation.wav")
+# print(text)
+# # print(text_to_keywords(text))
 # print(time.time() - start)
