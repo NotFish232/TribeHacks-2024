0327ca2403261b82f59ee156fcd8e11a3d628c58
Justin Lee <2025jlee@tjhsst.edu>
2024-04-20 18:47:39 -0400

    add cool spinny thing

--- static/js/game.js
+++ static/js/game.js
@@ -6,6 +6,9 @@ $(function () {
     let hammers_element = $("#hammers");
     let lives_element = $("#lives");
     let crab_element = $("#crab");
+    let streak_element = $("#streak")[0];
+
+    let streak = 0;
 
     let question_set;
     let question;
@@ -34,7 +37,7 @@ $(function () {
         let random_keys = [];
 
         while (random_keys.length != 4) {
-            let random_key = keys[keys.length * Math.random() << 0];
+            let random_key = keys[(keys.length * Math.random()) << 0];
             if (!random_keys.includes(random_key)) {
                 random_keys.push(random_key);
             }
@@ -44,9 +47,9 @@ $(function () {
         question = question_set[0];
         answer = json[question];
         question_set = question_set
-            .map(x => ({ x, key: Math.random() }))
+            .map((x) => ({ x, key: Math.random() }))
             .sort((a, b) => a.key - b.key)
-            .map(({ x }) => x)
+            .map(({ x }) => x);
 
         for (let i = 0; i < 4; ++i) {
             card_elements[i].innerHTML = json[question_set[i]];
@@ -59,21 +62,43 @@ $(function () {
             `<div class="left-[50%] top-[20%] absolute h-96 w-96"><img src="/static/assets/hammer.png"></div>`,
         );
 
-        let x = 275;
+        let x = 350;
         let y = 0;
+        let rotation = 180;
 
-        hammers.push([hammer_element, x, y]);
+        hammer_element.css("transform", `translate(${x}px, ${y}px)`);
+
+        hammers.push([hammer_element, x, y, rotation]);
         hammers_element.append(hammer_element);
-
     }
 
     function update_hammer_locations() {
         for (let i = 0; i < hammers.length; ++i) {
-            let [hammer_element, x, y] = hammers[i];
-            hammer_element.css("transform", `translate(${x}px, ${y}px)`);
+            let [hammer_element, x, y, rotation] = hammers[i];
+            hammer_element.css("transform", `translate(${x}px, ${y}px) rotate(${rotation}deg)`);
 
             hammers[i][2] += 2;
+            hammers[i][3] -= 0.5;
         }
+    }
+
+    function show_correct_answer() {
+        let full_card_elements = $("[id^=full_card_]");
+        for (let i = 0; i < 4; ++i) {
+            if (question == question_set[i]) {
+                $(full_card_elements.get(i)).addClass("border-green-800");
+            } else {
+                $(full_card_elements.get(i)).addClass("border-red-800");
+            }
+        }
+
+        setTimeout((_) => {
+            create_new_hammer();
+            set_next_question_set();
+
+            full_card_elements.removeClass("border-green-800");
+            full_card_elements.removeClass("border-red-800");
+        }, 1000);
     }
 
     function update_displayed_lives() {
@@ -102,24 +127,28 @@ $(function () {
     $("[id^=full_card_]").on("click", function (e) {
         let idx = parseInt($(this).attr("id").slice(-1)) - 1;
 
+        hammers[0][0].remove();
+        hammers = [];
+
         if (question == question_set[idx]) {
-            set_next_question_set();
+            streak += 1;
         } else {
-            hammers[0][0].remove();
-            hammers = [];
             num_lives -= 1;
             if (num_lives == 0) {
                 handle_game_over();
             }
-            create_new_hammer();
-            set_next_question_set();
+            streak = 0;
         }
+
+        show_correct_answer();
+
+        streak_element.innerHTML = `Streak: ${streak}`;
     });
 
     function game_loop() {
         update_hammer_locations();
 
-        for (let [hammer, _x, _y] of hammers) {
+        for (let [hammer, _x, _y, _r] of hammers) {
             if (are_colliding(crab_element, hammer)) {
                 hammers[0][0].remove();
                 hammers = [];
@@ -129,13 +158,14 @@ $(function () {
                 }
                 create_new_hammer();
                 set_next_question_set();
+
+                streak = 0;
+                streak_element.innerHTML = `Streak: ${streak}`;
             }
         }
 
         update_displayed_lives();
     }
-
-
 
     create_new_hammer();
 
