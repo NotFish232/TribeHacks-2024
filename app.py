2b9fcb25fab5c1b865d82e4195c71f2c7cdc79ca
Justin Lee <2025jlee@tjhsst.edu>
2024-04-20 12:33:24 -0400

    init website struct

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import os
+
+template_dir = os.path.abspath("./templates")
+app = Flask(__name__, template_folder=template_dir)
+
+
+@app.route("/", methods=["GET"])
+def main_page() -> str:
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", 8080)
