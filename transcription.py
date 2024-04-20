2d506420e38692193ee28f1235be5279e99a61bf
Alan Jiang <alanzjiang10@gmail.com>
2024-04-20 12:32:27 -0400

    Transcription finished for small files.

--- transcription.py
+++ transcription.py
@@ -1,2 +1,14 @@
-def audio_to_text(audio) -> str:
-    pass
+import speech_recognition as sr
+
+def audio_to_text(audioName) -> str:
+    r = sr.Recognizer()
+    with sr.AudioFile(audioName) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text = r.recognize_google(audio_data)
+        print(text)
+
+
+filename = "16-122828-0002.wav"
+audio_to_text(filename)
