4b10b956ede80b35917563db0871d1e9dbe5185c
Justin Lee <2025jlee@tjhsst.edu>
2024-04-20 12:39:35 -0400

    some updates

--- transcription.py
+++ transcription.py
@@ -1,14 +1,10 @@
 import speech_recognition as sr
 
-def audio_to_text(audioName) -> str:
+def audio_to_text(audio_name) -> str:
     r = sr.Recognizer()
-    with sr.AudioFile(audioName) as source:
-        # listen for the data (load audio to memory)
+    with sr.AudioFile(audio_name) as source:
         audio_data = r.record(source)
-        # recognize (convert from speech to text)
+      
         text = r.recognize_google(audio_data)
-        print(text)
 
-
-filename = "16-122828-0002.wav"
-audio_to_text(filename)
+        return text
