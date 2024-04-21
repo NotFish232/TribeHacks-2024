846dd5e8fca359d28fd2399e2d8354c68658cea6
Alan Jiang <alanzjiang10@gmail.com>
2024-04-20 22:09:09 -0400

    Updated Loading and app

--- src/app.py
+++ src/app.py
@@ -15,14 +15,16 @@ def main_page() -> str:
 @app.route("/loading", methods=["POST"])
 def loading_page() -> str:
     if 'file' in request.files: #File option
-        file = request.files['file']
+        file = request.files['file_input']
+        print("test2")
         # Check the file type (content type)
         # content_type = magic.from_buffer(file.read(2048), mime=True)
         file.save('../static/assets/' + file.filename)
         with open('filename.txt', 'w') as txt_file:
             txt_file.write(file.filename)
     else: #Text option
-        user_text = request.form['user_text']
+        print("test")
+        user_text = request.form['text_input']
         with open('user_text.txt', 'w') as txt_file:
             txt_file.write(user_text)
         with open('filename.txt', 'w') as txt_file:
@@ -30,20 +32,22 @@ def loading_page() -> str:
     return render_template("loading.html")
     
 
-@app.route('/parse_text', methods=["POST"])
+@app.route('/parse_text')
 def parse_text() -> str:
+    print("test3")
     with open('filename.txt', 'r') as txt_file:
         filename = txt_file.read()
     with open(filename, "rb") as file:
         text = ""
-        if "audio" in fleep.get(file).type: #audio file
+        if "audio" in fleep.get(file.read(128)).type: #audio file
             text = audio_to_text(filename)
         else:
-            text = file.read()
+            with open(filename, "r") as file2:
+                text = file2.read()
     keyword_pairs = text_to_keywords(text)
-    with open('data.json', 'w') as f:
-        json.dump(keyword_pairs, f)
-    return "text parsed"
+    # with open('data.json', 'w') as f:
+    #     json.dump(keyword_pairs, f)
+    return keyword_pairs
 
 @app.route("/game", methods=["GET"])
 def game_page() -> str:
