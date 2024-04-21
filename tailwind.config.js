b6802fe99389a92e7ad54441ea5c50a3ff1e4701
Justin Lee <2025jlee@tjhsst.edu>
2024-04-21 01:27:25 -0400

    add tailwind conf back

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./templates/*.html", "./static/js/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                "theme-halfw":"#ecf0f1",
+                "theme-artic": "#b0c0c7",
+                "theme-steel": "#3a6383",
+                "theme-slate": "#2f4253",
+                "theme-prussian": "#15212d",
+                "theme-bronze": "#d09a6e",
+            },
+        },
+    },
+    plugins: [],
+};
